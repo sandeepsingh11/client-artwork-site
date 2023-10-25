fd8688789aee606594bf7df935729923816643cb
Sandeep Singh <serperior2@protonmail.com>
2023-10-24 22:24:57 -0400

    init commit

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+const yaml = require("js-yaml");
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
+
+    eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("src/scripts");
+
+    // Input Directory : src, Output directory: _site
+    return {
+        dir: { input: 'src', output: '_site' }
+    };
+};
