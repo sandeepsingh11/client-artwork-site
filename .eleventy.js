17fc8d7479162e0e195854b1107e00a9580f3fc8
Sandeep Singh <serperior2@protonmail.com>
2023-11-15 18:58:57 -0500

    add Decap CMS, tweak data flow, add samples to CMS

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,11 @@ const yaml = require("js-yaml");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
+    eleventyConfig.addDataExtension("yml", contents => yaml.load(contents));
 
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("src/scripts");
+    eleventyConfig.addPassthroughCopy("src/admin");
 
     // Input Directory : src, Output directory: _site
     return {
